a0b2160715ce2136e078cb1cd579c4b727196496
A Vinay Kumar <aeddua.vinay@gmail.com>
2026-06-29 12:09:35 +0530

    Add logging system

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from agents.project_manager_agent import ProjectManagerAgent
+from utils.logger import app_logger
+
 
 def main():
+    app_logger.info("ProjectOS Started")
+
     print("Welcome to ProjectOS AI Agents")
     print("--------------------------------")
 
     user_input = input("Enter your project idea: ")
 
     manager = ProjectManagerAgent()
+
     result = manager.run(user_input)
+
+    app_logger.info("Project created successfully")
 
     print("\nResult:")
     print(result)
+
 
 if __name__ == "__main__":
     main()
